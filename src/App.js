618678e1162b83f30d9fb6702e635bd2c5282f05
Burak <burakkayakoc@gmail.com>
2020-03-31 20:43:10 -0600

    added functions by following the react-usa-map package

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css'; /* optional for styling like the :hover pseudo-class */
 import USAMap from "react-usa-map";
 
 class App extends Component {
@@ -7,10 +8,23 @@ class App extends Component {
     alert(event.target.dataset.name);
   };
 
+  /* optional customization of filling per state and calling custom callbacks per state */
+  statesCustomConfig = () => {
+    return {
+      "NJ": {
+        fill: "navy",
+        clickHandler: (event) => console.log('Custom handler for NJ', event.target.dataset)
+      },
+      "NY": {
+        fill: "#CC0000"
+      }
+    };
+  };
+
   render() {
     return (
       <div className="App">
-        <USAMap onClick={this.mapHandler} />
+        <USAMap customize={this.statesCustomConfig()} onClick={this.mapHandler} />
       </div>
     );
   }

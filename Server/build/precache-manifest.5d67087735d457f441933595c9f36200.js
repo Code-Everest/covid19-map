d8c1b060891903f64e4836e832bc65241bbb267f
Burak <burakkayakoc@gmail.com>
2020-04-01 05:59:52 -0600

    testing express deployment

---
+++ Server/build/precache-manifest.5d67087735d457f441933595c9f36200.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128375f37a5112dd522ecee048fb06d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcd1a9012f2be93e98e2",
+    "url": "/static/css/main.46c7fcf8.chunk.css"
+  },
+  {
+    "revision": "861c234e4908d9df1441",
+    "url": "/static/js/2.02b80086.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.02b80086.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcd1a9012f2be93e98e2",
+    "url": "/static/js/main.199c0f8c.chunk.js"
+  },
+  {
+    "revision": "f5847aaf3e4ed6afd9ab",
+    "url": "/static/js/runtime-main.6f9736bb.js"
+  }
+]);

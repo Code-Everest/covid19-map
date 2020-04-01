46dfccfd7ee69a98d4dbd35b5b49e407037da3b8
Burak <burakkayakoc@gmail.com>
2020-03-31 20:42:21 -0600

    imported the react-usa=-map package app.js file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import USAMap from "react-usa-map";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  /* mandatory */
+  mapHandler = (event) => {
+    alert(event.target.dataset.name);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <USAMap onClick={this.mapHandler} />
+      </div>
+    );
+  }
 }
 
 export default App;

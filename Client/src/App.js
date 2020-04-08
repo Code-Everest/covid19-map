790e1d75a5aa3abf8b8adc710736978f38a7a87c
Burak <burakkayakoc@gmail.com>
2020-04-08 15:46:20 -0600

    organized the folder/files, created Map files

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,58 +1,17 @@
 import React, { Component } from 'react';
-import './App.css'; /* optional for styling like the :hover pseudo-class */
-import USAMap from "react-usa-map";
-
-import GET from './data/GET';
-
-
-
+import './App.css';
+import Map from "./components/Map"
 
 class App extends Component {
 
-state = {TotalCases : ""
-}
-
-
-  /* mandatory */
-  mapHandler = (event) => {
-    alert(event.target.dataset.name);
-  };
-
-  /* optional customization of filling per state and calling custom callbacks per state */
-  statesCustomConfig = () => {
-    return {
-      "NJ": {
-        fill: "navy",
-        clickHandler: (event) => console.log('Custom handler for NJ', event.target.dataset)
-      },
-      "NY": {
-        fill: "#CC0000"
-      }
-    };
-  };
-
-  calculatePercentage = (num1, num2) =>{
-    return console.log(num1*100/num2)
-  }
  
-
-    
-  componentDidMount() {
-    GET.getTotalCases()
-    GET.getAllStates()
-    GET.getOneStateCase("Alabama")
-  }
 
 
   render() {
-    
+
     return (
-
-      <div className="App">
-        <USAMap customize={this.statesCustomConfig()} onClick={this.mapHandler} />
-      </div>
-
-    );
+     <Map />
+    )
   }
 }
 

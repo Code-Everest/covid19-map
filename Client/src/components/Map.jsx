b83082b27709d1b4db7c17dc57466ab41225f934
Burak <burakkayakoc@gmail.com>
2020-04-17 22:08:27 -0600

    added foreach function for listing the case numbers

--- Client/src/components/Map.jsx
+++ Client/src/components/Map.jsx
@@ -22,98 +22,115 @@ class Map extends Component {
 
   state = {
     Provinces: "",
-    Provinces2: { States:["Alabama"]},
-    newstates:""
-  };
+    Provinces2: {
+      States: {
+        Alabama: "",
+        Tennessee: ""
+      },
+      newstates: 1
+    };
 
-  /* mandatory */
-  mapHandler = (event) => {
-    alert(event.target.dataset.name);
-  };
+    /* mandatory */
+    mapHandler = (event) => {
+      alert(event.target.dataset.name);
+    };
+
+    /* optional customization of filling per state and calling custom callbacks per state */
+
+    statesCustomConfig = () => {
 
 
-
-  /* optional customization of filling per state and calling custom callbacks per state */
-
-
-  statesCustomConfig = () => {
-
-    return {
-      "Alabama": {
-        fill: "BLUE"
+      return {
+        "Alabama": {
+          fill: "BLUE"
+        },
+        "CA": {
+          fill: "BLUE"
+        }
       }
-    }
-  };
+    };
 
-  calculatePercentage = (num1, num2) => {
-    return console.log(num1 * 100 / num2)
-  }
+    calculatePercentage = (num1, num2) => {
+      return console.log(num1 * 100 / num2)
+    }
 
   returnMap = () => {
-    return (
-      <div className="App">
-        <USAMap customize={this.statesCustomConfig()} onClick={this.mapHandler} />
-      </div>
-    )
-  }
+      return (
+        <div className="App">
+          <USAMap customize={this.statesCustomConfig()} onClick={this.mapHandler} />
+        </div>
+      )
+    }
+
+
+
 
 
   componentDidMount() {
 
-    req.end(res => {
-      if (res.error) throw new Error(res.error);
+      req.end(res => {
+        if (res.error) throw new Error(res.error);
 
-      const CovidData = res.body.data.covid19Stats
+        const CovidData = res.body.data.covid19Stats
 
-      
-        this.setState({newstates : [{data}].name  }) 
-      
-     // console.log(this.state.newstates)
+        this.setState({ newstates: [{ data }].name })
 
-      //STATES
-      const States = "Alabama"
-      this.setState({ Provinces: States })
+        // console.log(this.state.newstates)
 
-      //TOTAL CASES
-      const confirmedCases = CovidData.map((e) => e.confirmed)
-      const totalCases = confirmedCases.reduce((partial_sum, a) => partial_sum + a, 0)
-      this.setState({ TotalCases: totalCases })
+        //STATES
+        const States = "Alabama"
+        this.setState({ Provinces: States })
 
-
-
-      const State = "Kentucky"
-      const filteredState = CovidData.filter(function (el) {
-        return el.province === State
-      });
-      const confirmed = filteredState.map(e => e.confirmed)
-      const totalCasesforState = confirmed.reduce((partial_sum, a) => partial_sum + a, 0)
-
-      //console.log("Total Cases for " + State + ": " + totalCasesforState)
-      //console.log(filteredState)
+        //TOTAL CASES
+        const confirmedCases = CovidData.map((e) => e.confirmed)
+        const totalCases = confirmedCases.reduce((partial_sum, a) => partial_sum + a, 0)
+        this.setState({ TotalCases: totalCases })
 
 
 
+        const State = ["Kentucky", "Alabama"]
+        const filteredState = CovidData.filter(function (el) {
+          return el.province === State
+        });
+        const confirmed = filteredState.map(e => e.confirmed)
+        const totalCasesforState = confirmed.reduce((partial_sum, a) => partial_sum + a, 0)
 
-      console.log("Total Cases: " + totalCasesforState)
-      console.log(this.state.Provinces)
+        State.forEach(function (e) {
+          const filteredState = CovidData.filter(function (el) {
+            return el.province === e
+          });
+          const confirmed = filteredState.map(e => e.confirmed)
+          const totalCasesforState = confirmed.reduce((partial_sum, a) => partial_sum + a, 0)
+          console.log(totalCasesforState)
+
+        })
+
+        //console.log("Total Cases for " + State + ": " + totalCasesforState)
+        //console.log(filteredState)
+
+
+
+
+        console.log("Total Cases: " + totalCasesforState)
+        //console.log(this.state.Provinces)
+      }
+      )
+
+      this.returnMap()
+
+
     }
-    )
-
-    this.returnMap()
-
-
-  }
 
 
   render() {
-    return (
-      <div className="App">
-        <USAMap customize= {this.statesCustomConfig()} onClick={this.mapHandler} />
-      </div>
-    )
+      return (
+        <div className="App">
+          <USAMap customize={this.statesCustomConfig()} onClick={this.mapHandler} />
+        </div>
+      )
 
 
+    }
   }
-}
 
-export default Map;
+  export default Map;
